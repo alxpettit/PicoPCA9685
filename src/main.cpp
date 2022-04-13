469b6661bcdb1161001cc66ae1c2b339d589dcbc
Alexandria <alxpettit@gmail.com>
2022-04-13 11:08:31 -0700

    sdiusdhusdihosiduhsdouhsdousdh

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,97 @@
-#include <Arduino.h>
-#include <PCA9685.h>
-#include <Wire.h>
+#include "PCA9685.h"
 
-// You can import the full Pico SDK. Its installation is handled by PlatformIO :)
-#include <pico.h>
+PCA9685 pwmController;         // Library using B010101 (A5-A0) i2c address, and default Wire @400kHz
 
-// https://community.element14.com/products/arduino/arduino-projects/b/blog/posts/arduino-nano-rp2040-connect---mqtt
+void setup() {
+    Serial.begin(115200);               // Begin Serial and Wire interfaces
+    // If it's allowed to default to default, it will disrupt serial comm I think
+    //Wire.setSCL(14u);
+    //Wire.setSDA(15u);
+    Wire.begin();
+    pinMode(PIN_LED, OUTPUT);
+    pwmController.resetDevices();       // Resets all PCA9685 devices on i2c line
 
-// This doesn't work
-// It seems to keep using I2C0, which uses up the TX/RX pins and causes the LED on pin 25 to flash
-// 1 1 1 1 2 2 2 2 1 1 1 1 2 2 2 2 1 1 1 1...
-// slow, fast, slow fast...
+    pwmController.init();               // Initializes module using default totem-pole driver mode, and default phase balancer
 
-// WHY?
-// I've tried just about everything... I need an example of someone getting I2C working with this...
-MbedI2C I2C(MBED_I2C1);
+    pwmController.setPWMFrequency(500); // Set PWM freq to 500Hz (default is 200Hz, supports 24Hz to 1526Hz)
 
-PCA9685 pwmController(I2C);
-PCA9685_ServoEval pwmServo1;
-
-__attribute__((unused)) void setup() {
-    Serial.begin(115200);
-    pwmController.resetDevices();
-    pwmController.init();
-    pwmController.setPWMFreqServo(); // 50Hz
-
-    pwmController.setChannelPWM(0, pwmServo1.pwmForAngle(90));
+    randomSeed(analogRead(0));          // Use white noise for our randomness
 }
 
-__attribute__((unused)) void loop() {
-    Serial.println("uwu");
+bool led_state = false;
+void loop() {
+    uint16_t pwms[12];
+    pwms[0] = random(0, 4096);
+    pwms[1] = random(0, 4096);
+    pwms[2] = random(0, 4096);
+    pwms[3] = random(0, 4096);
+    pwms[4] = random(0, 4096);
+    pwms[5] = random(0, 4096);
+    pwms[6] = random(0, 4096);
+    pwms[7] = random(0, 4096);
+    pwms[8] = random(0, 4096);
+    pwms[9] = random(0, 4096);
+    pwms[10] = random(0, 4096);
+    pwms[11] = random(0, 4096);
+    pwmController.setChannelsPWM(0, 12, pwms);
+    Serial.println(pwms[0]);
+    digitalWrite(PIN_LED, led_state);
+    led_state = !led_state;
+    delay(100);
+
+    // NOTE: Many chips use a BUFFER_LENGTH size of 32, and in that case writing 12
+    // channels will take 2 i2c transactions because only 7 channels can fit in a single
+    // i2c buffer transaction at a time. This may cause a slight offset flicker between
+    // the first 7 and remaining 5 channels, but can be offset by experimenting with a
+    // channel update mode of PCA9685_ChannelUpdateMode_AfterAck. This will make each
+    // channel update immediately upon sending of the Ack signal after each PWM command
+    // is executed rather than at the Stop signal at the end of the i2c transaction.
 }
+
+
+
+//#include <Arduino.h>
+//#include <PCA9685.h>
+//#include <Wire.h>
+//
+//// You can import the full Pico SDK. Its installation is handled by PlatformIO :)
+//#include <pico.h>
+//
+//// https://community.element14.com/products/arduino/arduino-projects/b/blog/posts/arduino-nano-rp2040-connect---mqtt
+//
+//// This doesn't work
+//// It seems to keep using I2C0, which uses up the TX/RX pins and causes the LED on pin 25 to flash
+//// 1 1 1 1 2 2 2 2 1 1 1 1 2 2 2 2 1 1 1 1...
+//// slow, fast, slow fast...
+//
+//// WHY?
+//
+//#define PIN_TEST PIN_LED
+//
+//PCA9685* pwmController;
+//PCA9685_ServoEval* pwmServo1;
+//
+//__attribute__((unused)) void setup() {
+//    pinMode(PIN_TEST, OUTPUT);
+//    Wire.begin();
+//    pwmServo1 = new PCA9685_ServoEval();
+//    pwmController = new PCA9685(Wire);
+//    Serial.begin(115200);
+//    pwmController->resetDevices();
+//    pwmController->init();
+//    pwmController->setPWMFreqServo(); // 50Hz
+//}
+//
+//bool status = false;
+//__attribute__((unused)) void loop() {
+//    digitalWrite(PIN_TEST, status);
+//    status = !status;
+//    for (int angle=0;angle<180;angle++) {
+//        for (int i = 0; i <= 15; ++i) {
+//            pwmController->setChannelPWM(i, pwmServo1->pwmForAngle(angle));
+//        }
+//        delay(1000);
+//    }
+//    delay(1000);
+//    Serial.println("uwu");
+//}
